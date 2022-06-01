d835ed6c5089f75ca069cb9a7b0eaa89f90cfc3a
Surina Saha <surina.saha@zemosolabs.com>
2022-06-01 17:40:10 +0530

    entered sonar properties and workflows

---
+++ src/test/java/com/zemoso/springbootassignment/controller/HotelsControllerTest.java
@@ -0,0 +1,44 @@
+package com.zemoso.springbootassignment.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HotelsControllerTest {
+
+    @Test
+    void listHotels() {
+    }
+
+    @Test
+    void showFormForAdd() {
+    }
+
+    @Test
+    void showFormForUpdate() {
+    }
+
+    @Test
+    void showFormForUpdateDto() {
+    }
+
+    @Test
+    void saveHotels() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void search() {
+    }
+
+    @Test
+    void reviews() {
+    }
+
+    @Test
+    void addReview() {
+    }
+}
